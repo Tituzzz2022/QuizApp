e18965f6e247809152d44b6dbde67aad2cf85d22
Tituzzz2022 <dhetzer3@gmail.com>
2023-02-04 17:58:44 +0100

    json to script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+let questions = [
+    {
+        question: "Wie heißt die Hauptstadt von Deutschland?",
+        "answer_1": "Berlin",
+        "answer_2": "Hamburg",
+        "answer_3": "München",
+        "answer_4": "Köln",
+        correctAnswer: "answer_1"
+    },
+    {
+        question: "Wie heißt die Hauptstadt von Frankreich?",
+        "answer_1": "Paris",
+        "answer_2": "Marseille",
+        "answer_3": "Lyon",
+        "answer_4": "Toulouse",
+        correctAnswer: "answer_1"
+    },
+    {
+        question: "Wie heißt die Hauptstadt von Italien?",
+        "answer_1": "Rom",
+        "answer_2": "Mailand",
+        "answer_3": "Neapel",
+        "answer_4": "Turin",
+        correctAnswer: "answer_1"
+    },
+    {
+        question: "Wie heißt die Hauptstadt von Spanien?",
+        "answer_1": "Madrid",
+        "answer_2": "Barcelona",
+        "answer_3": "Valencia",
+        "answer_4": "Sevilla",
+        correctAnswer: "answer_1"
+    },
+ 
+];
