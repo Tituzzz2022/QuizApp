b3b5a99ce7b6ef338c5599dc22fec106abdd9eaf
Daniel Bergmann <dhetzer3@gmail.com>
2023-02-05 15:25:09 +0100

    firt Commit

--- script.js
+++ script.js
@@ -1,48 +1,73 @@
 let questions = [
-    {
-        question: "Wie heißt die Hauptstadt von Deutschland?",
-        "answer_1": "Berlin",
-        "answer_2": "Hamburg",
-        "answer_3": "München",
-        "answer_4": "Köln",
-        correctAnswer: "answer_1"
-    },
-    {
-        question: "Wie heißt die Hauptstadt von Frankreich?",
-        "answer_1": "Paris",
-        "answer_2": "Marseille",
-        "answer_3": "Lyon",
-        "answer_4": "Toulouse",
-        correctAnswer: "answer_1"
-    },
-    {
-        question: "Wie heißt die Hauptstadt von Italien?",
-        "answer_1": "Rom",
-        "answer_2": "Mailand",
-        "answer_3": "Neapel",
-        "answer_4": "Turin",
-        correctAnswer: "answer_1"
-    },
-    {
-        question: "Wie heißt die Hauptstadt von Spanien?",
-        "answer_1": "Madrid",
-        "answer_2": "Barcelona",
-        "answer_3": "Valencia",
-        "answer_4": "Sevilla",
-        correctAnswer: "answer_1"
-    },
- 
+  {
+    question: "Wie heißt die Hauptstadt von Deutschland?",
+    answer_1: "Berlin",
+    answer_2: "Hamburg",
+    answer_3: "München",
+    answer_4: "Köln",
+    correctAnswer: 1
+  },
+  {
+    question: "Wie heißt die Hauptstadt von Frankreich?",
+    answer_1: "Paris",
+    answer_2: "Marseille",
+    answer_3: "Lyon",
+    answer_4: "Toulouse",
+    correctAnswer: 1
+  },
+  {
+    question: "Wie heißt die Hauptstadt von Italien?",
+    answer_1: "Rom",
+    answer_2: "Mailand",
+    answer_3: "Neapel",
+    answer_4: "Turin",
+    correctAnswer: 1,
+  },
+  {
+    question: "Wie heißt die Hauptstadt von Spanien?",
+    answer_1: "Madrid",
+    answer_2: "Barcelona",
+    answer_3: "Valencia",
+    answer_4: "Sevilla",
+    correctAnswer: 1,
+  },
 ];
 
-let currentQuestion = 1;
+let currentQuestion = 0;
 
 function init() {
-    document.getElementById('all-questions').innerHTML = questions.length;
-
+  document.getElementById("all-questions").innerHTML = questions.length;
+  showQuestion();
 }
 
 //Frage anzeigen
 function showQuestion() {
-  let question = questions[currentQuestion];
+  let question_container = questions[currentQuestion];
+  document.getElementById("questiontext").innerHTML =
+    question_container.question;
+  document.getElementById("answer_1").innerHTML = question_container.answer_1;
+  document.getElementById("answer_2").innerHTML = question_container.answer_2;
+  document.getElementById("answer_3").innerHTML = question_container.answer_3;
+  document.getElementById("answer_4").innerHTML = question_container.answer_4;
+}
+
+function answer(index) {
+  let question_container = questions[currentQuestion];
+  console.log('Current Question is', question_container.question);
+  console.log("selected answer is ", index);
+
+  let selectedQuestionNumber = index.slice(-1);
+  console.log("selected question number is ", selectedQuestionNumber);
+  console.log('Correct Answer is', question_container.correctAnswer);
+
+  let correctAnswerID = `answer_${question_container.correctAnswer}`;
+
+  if (selectedQuestionNumber == question_container.correctAnswer) {
+    document.getElementById(index).parentNode.classList.add("bg-success");
 
 
+  } else {
+    document.getElementById(index).parentNode.classList.add("bg-danger");
+    document.getElementById(correctAnswerID).parentNode.classList.add("bg-success");
+  }
+}
